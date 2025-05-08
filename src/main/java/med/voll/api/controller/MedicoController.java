7ea8d2855db6c8611b4857a02c8cf2c5046d8a6b
Lucas Costa <lc.benjamin@gmail.com>
2025-05-08 15:20:11 -0300

    Commit inicial

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,63 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.medico.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping(("/medicos"))
+public class MedicoController {
+
+    private final MedicoRepository repository;
+
+    public MedicoController(MedicoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    @PostMapping
+    public ResponseEntity<DadosDetalhamentoMedico> cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medico/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosDetalhamentoMedico> detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok((new DadosDetalhamentoMedico(medico)));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @Transactional
+    @PutMapping
+    public ResponseEntity<DadosDetalhamentoMedico> atualizar(@RequestBody DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/med/voll/api/infra/DadosTokenJwt.java
@@ -0,0 +1,4 @@
+package med.voll.api.infra;
+
+public record DadosTokenJwt(String token) {
+}
